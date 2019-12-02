414164bfb0b2398f3ff70b6addfb701cb2b27813
Clara Fitzgerald <clara.fitzgerald@Nology.local>
2019-12-02 16:59:22 +0000

    secured API key and deployed

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Event.module.scss";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Event extends Component {
   state = {};
@@ -25,18 +27,20 @@ class Event extends Component {
   render() {
     return (
       <section className={styles.main}>
-        <p className={styles.title}>{this.props.summary}</p>
-        <i className={styles.dates}>
-          {this.renderDateAndTime(this.props.startDate)} until{" "}
-          {this.renderDateAndTime(this.props.endDate)}
-        </i>
+        <div>
+          <p className={styles.title}>{this.props.summary}</p>
+          <i className={styles.dates}>
+            {this.renderDateAndTime(this.props.startDate)} until{" "}
+            {this.renderDateAndTime(this.props.endDate)}
+          </i>
+        </div>
         <a
           className={styles.link}
           href={this.props.link}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open with Google Calendar
+          Open with Google Calendar <FontAwesomeIcon icon={faExternalLinkAlt} />
         </a>
       </section>
     );
